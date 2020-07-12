3de299c0477523e71ce86db23fc4a8a750e8004a
Eric Park <ideamaneric@gmail.com>
2020-07-12 12:16:07 +0800

    dreamlte: inherit some common Bliss stuff

--- bliss_dreamlte.mk
+++ bliss_dreamlte.mk
@@ -22,7 +22,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 $(call inherit-product, device/samsung/dreamlte/device.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
+# Inherit some common Bliss stuff.
 $(call inherit-product, vendor/bliss/config/common_full_phone.mk)
 
 # Bootanimation
